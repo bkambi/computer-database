4b547480bf90afa8f86a162d0aa074f510b8dab3
bkambi <bkambi@excilys.com>
2019-03-21 10:47:16 +0100

    feat : init web-ui

--- src/main/java/com/excilys/cdb/controller/ComputerController.java
+++ src/main/java/com/excilys/cdb/controller/ComputerController.java
@@ -4,7 +4,6 @@ import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
--- src/main/java/com/excilys/cdb/controller/ComputerServlet.java
+++ src/main/java/com/excilys/cdb/controller/ComputerServlet.java
@@ -1,6 +1,7 @@
 package com.excilys.cdb.controller;
 
 import java.io.IOException;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,19 +9,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class EditComputerServlet
+ * Servlet implementation class ComputerServlet
  */
-@WebServlet("/EditComputerServlet")
-public class EditComputerServlet extends HttpServlet {
+@WebServlet("/computer")
+public class ComputerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public EditComputerServlet() {
+    public ComputerServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -36,6 +44,10 @@ public class EditComputerServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+	}
+	
+	private void handleRequestForListComputer() {
+		
 	}
 
 }
